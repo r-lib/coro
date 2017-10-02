4667ccf55464050358a3165e345dcf706634884b
Lionel Henry <lionel.hry@gmail.com>
2017-10-02 14:14:48 +0200

    Rename mut_node_tail_cdr() to node_list_poke_cdr()
    - Same for car version
    - Rename node_tail() to node_list_tail()

--- R/rlang.R
+++ R/rlang.R
@@ -3,18 +3,21 @@ dots_node <- function(...) {
   node_cdr(substitute(lang(...)))
 }
 
-node_tail <- function(node) {
-  while(!is_null(node_cdr(node))) {
+node_list_tail <- function(node) {
+  while (!is_null(node_cdr(node))) {
     node <- node_cdr(node)
   }
   node
 }
 
-mut_node_tail_car <- function(node, new_car) {
-  mut_node_car(node_tail(node), new_car)
+node_list_poke_car <- function(node, new_car) {
+  node_poke_car(node_list_tail(node), new_car)
 }
-mut_node_tail_cdr <- function(node, new_cdr) {
-  mut_node_cdr(node_tail(node), new_cdr)
+node_list_poke_cdr <- function(node, new_cdr) {
+  node_poke_cdr(node_list_tail(node), new_cdr)
 }
 
 is_reference <- rlang:::is_reference
+
+node_poke_car <- mut_node_car
+node_poke_cdr <- mut_node_cdr
