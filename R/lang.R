78426d6cba4c7f711b34fc2cef26da7ad5456755
Lionel Henry <lionel.hry@gmail.com>
2017-10-02 17:48:51 +0200

    Remove shift-reset operators
    Switch to a less flexible but more efficient translation to machine
    state strategy

--- R/lang.R
+++ R/lang.R
@@ -1,9 +1,4 @@
 
-is_jump <- function(x) {
-  is_lang(x, jump_syms)
-}
-jump_syms <- list(quote(`break`), quote(`next`))
-
 lang_has <- function(lang, is_element) {
   if (!is_language(lang)) {
     return(FALSE)
@@ -23,40 +18,14 @@ lang_has <- function(lang, is_element) {
   FALSE
 }
 
-has_shift <- function(expr) {
-  lang_has(expr, function(x) is_shift(x) || is_assigned_shift(x))
-}
-is_shift <- function(expr) {
-  is_language(expr, shift_sym)
-}
-is_assigned_shift <- function(expr) {
-  is_language(expr, quote(`<-`)) && is_shift(node_cadr(node_cdr(expr)))
-}
-
-lang_check_shift <- function(lang) {
-  if (is_assigned_shift(lang)) {
-    return(NULL)
+as_exprs_node <- function(expr) {
+  if (is_pairlist(expr)) {
+    expr
+  } else if (is_language(expr, quote(`{`))) {
+    node_cdr(expr)
+  } else {
+    node(expr, NULL)
   }
-
-  check_shift(node_car(lang))
-
-  args <- node_cdr(lang)
-  while (!is_null(args)) {
-    car <- node_car(args)
-    args <- node_cdr(args)
-
-    check_shift(car)
-  }
-}
-check_shift <- function(expr) {
-  if (!is_language(expr)) {
-    return(NULL)
-  }
-
-  if (is_shift(expr)) {
-    abort("Can't shift within a function call")
-  }
-  lang_check_shift(expr)
 }
 
 as_block_lang <- function(x) {
@@ -66,18 +35,11 @@ as_block_lang <- function(x) {
   x
 }
 
-
-bangbang <- function(expr) {
-  lang(quote(UQ), expr)
-}
-
 ctrl_syms <- list(
   quote(`if`),
   quote(`for`),
   quote(`while`),
   quote(`repeat`)
 )
-next_sym <- quote(`_next`)
-shift_sym <- quote(SHIFT)
 assignment_sym <- quote(`<-`)
 block_sym <- quote(`{`)
--- tests/testthat/helper-cnt.R
+++
@@ -1,4 +0,0 @@
-
-cnt_body <- function(cnt) {
-  node_cadr(body(cnt))
-}
