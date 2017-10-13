e88cfcdd2dd0f848f13fa5c1777be04e02f644f6
Lionel Henry <lionel.hry@gmail.com>
2017-10-13 16:49:30 +0200

    Handle `next` and `break` within iterate()

--- R/iterator.R
+++ R/iterator.R
@@ -121,6 +121,7 @@ iterate <- function(expr, env = caller_env()) {
 
   elt_name <- as_string(node_car(args))
   loop_expr <- node_cadr(node_cdr(args))
+  loop_expr <- poke_loop_keywords(loop_expr)
 
   if (is_stream_iterator(iter)) {
     not_done <- function(iter) !is_null(iter())
@@ -132,8 +133,64 @@ iterate <- function(expr, env = caller_env()) {
 
   while (not_done(iter)) {
     env_poke(env, elt_name, elt(iter))
-    eval_bare(loop_expr, env)
+    out <- with_loop_flow(eval_bare(loop_expr, env))
+    switch(out,
+      `next` = next,
+      `break` = break
+    )
   }
 
   invisible(NULL)
+}
+
+poke_loop_keywords <- function(node) {
+  rest <- node
+
+  while (TRUE) {
+    rest <- node_cdr(rest)
+    if (is_null(rest)) {
+      break
+    }
+
+    current <- node_car(rest)
+    if (!is_language(current) || !is_symbol(node_car(current))) {
+      next
+    }
+
+    head <- node_car(current)
+    switch(as_string(head),
+      # Don't poke within nested loops
+      `repeat` = ,
+      `while` = ,
+      `for` = {
+        next
+      },
+      `next` = {
+        node_poke_car(rest, flow_signaller("flowery_next"))
+        next
+      },
+      `break` = {
+        node_poke_car(rest, flow_signaller("flowery_break"))
+        next
+      },
+      poke_loop_keywords(current)
+    )
+  }
+
+  invisible(node)
+}
+
+flow_signaller <- function(type) {
+  lang(function() cnd_signal(type))
+}
+
+with_loop_flow <- function(expr, env = caller_env()) {
+  tryCatch(
+    flowery_next = function(cnd) "next",
+    flowery_break = function(cnd) "break",
+    {
+      expr
+      "next"
+    }
+  )
 }
--- tests/testthat/test-iterator.R
+++ tests/testthat/test-iterator.R
@@ -116,3 +116,34 @@ test_that("can iterate() with over batch iterators", {
   })
   expect_identical(out, 1:3)
 })
+
+test_that("can use break within iterate()", {
+  out <- int()
+  bool <- FALSE
+
+  iterate(for (x in 1:3) {
+    out <- c(out, x)
+    if (bool) {
+      break
+    }
+    bool <- TRUE
+  })
+
+  expect_identical(out, 1:2)
+})
+
+test_that("can use next within iterate()", {
+  out <- int()
+  bool <- FALSE
+
+  iterate(for (x in 1:3) {
+    if (bool) {
+      next
+    } else {
+      out <- c(out, x)
+    }
+    bool <- TRUE
+  })
+
+  expect_identical(out, 1L)
+})
