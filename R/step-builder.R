4d97e32750f6cfff31b5653c46217439564109ef
Lionel Henry <lionel.hry@gmail.com>
2017-10-25 18:44:56 +0200

    Export and document builder functions

--- R/step-builder.R
+++ R/step-builder.R
@@ -1,8 +1,58 @@
-
-into_builder <- function(to) {
-  poke_into_builder(duplicate(to, shallow = TRUE))
+#' Build a typed output
+#'
+#' @description
+#'
+#' `along_builder()` and `poke_into_builder()` are builder functions,
+#' i.e. reducer functions that construct an output when reduced with
+#' [purrr::reduce()] or [reduce_steps()]. While `along_builder()`
+#' duplicates its input, `poke_into_builder()` fills the supplied
+#' input in place. The latter is only meant for experience users as
+#' this mutation can cause unexpected side effects. You should only
+#' mutate vectors that you have created.
+#'
+#' These builder functions construct a vector of a given type and
+#' manage memory. The vector is automatically grown (with a rate of
+#' 1.5) when it becomes too small and filled with the output during
+#' reduction. The vector is automatically shrinked to the final output
+#' size when the reduction completes. To avoid the extra copies caused
+#' by vector growing and shrinking, supply a vector whose size matches
+#' exactly the final output.
+#'
+#' The builder functions can be combined with [transformation
+#' steps][steps] to provide interesting functionality. See the
+#' [take()] variants for higher-level functions.
+#'
+#' @param along A vector that will be safely duplicated before
+#'   filling.
+#' @param to A vector that will be modified in place. If the final
+#'   output has a different length than `to`, it will still be copied
+#'   when grown and/or shrunk.
+#'
+#' @seealso [reduce_steps()], [steps]
+#' @export
+#' @examples
+#' # Let's discard all pair elements during reduction to make the
+#' # examples a bit more interesting
+#' trans <- keep_step(~ . %% 2 == 0)
+#'
+#' # You can supply an empty vector to indicate the output type. The
+#' # output vector will be grown efficiently.
+#' reduce_steps(1:10, trans, along_builder(list()))
+#'
+#' # If you have created a vector and want to modify it in place to
+#' # gain a bit of performance, you can use the poker version:
+#' vec <- vector("list", 5L)
+#' reduce_steps(1:10, trans, poke_into_builder(vec))
+#'
+#' # The output vector is modified in place. This can have surprising
+#' # side effects:
+#' vec
+along_builder <- function(along) {
+  to <- duplicate(along, shallow = TRUE)
+  poke_into_builder(to)
 }
-
+#' @rdname along_builder
+#' @export
 poke_into_builder <- function(to) {
   stopifnot(is_bare_vector(to))
 
