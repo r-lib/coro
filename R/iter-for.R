d9c2caa367c37d73ff0c8df574c6f87a11485541
Lionel Henry <lionel.hry@gmail.com>
2017-10-26 17:44:26 +0200

    Use state machine approach in iterate()

--- R/iter-for.R
+++ R/iter-for.R
@@ -4,83 +4,37 @@ iterate <- function(loop) {
   if (!is_language(loop, for_sym)) {
     abort("`loop` must be a `for` loop")
   }
+
   env <- caller_env()
-
   args <- node_cdr(loop)
-  it <- eval_bare(node_cadr(args), env)
-  it <- as_iterator(it)
 
-  if (!is_iterator(it)) {
-    abort("`iterate()` expects a loop over an iterator")
-  }
+  elt <- node_car(args)
+  coll <- node_cadr(args)
+  expr <- node_cadr(node_cdr(args))
 
-  elt_name <- as_string(node_car(args))
-  loop_expr <- node_cadr(node_cdr(args))
-  loop_expr <- poke_loop_keywords(loop_expr)
-
-  not_done <- function(it) !is_null(it())
-  elt <- function(it) deref(it)
-
-  while (!(is_null(it()) && is_done(it))) {
-    env_poke(env, elt_name, deref(it))
-    out <- with_loop_flow(eval_bare(loop_expr, env))
-    switch(out,
-      `next` = next,
-      `break` = break
-    )
-  }
-
-  invisible(NULL)
+  iter_for(!! elt, coll, !! expr, env = env)
 }
 
-poke_loop_keywords <- function(node) {
-  rest <- node
+iter_for <- function(elt, coll, expr, env = caller_env()) {
+  lang <- for_lang(enexpr(elt), coll, enexpr(expr))
 
-  while (TRUE) {
-    rest <- node_cdr(rest)
-    if (is_null(rest)) {
-      break
-    }
+  # Translate `for` loop to machine state
+  reset_state()
+  node <- as_exprs_node(lang)
+  parts <- node_list_parts(node)
 
-    current <- node_car(rest)
-    if (!is_language(current) || !is_symbol(node_car(current))) {
-      next
-    }
+  # Add breaking state to state machine
+  node_list_poke_cdr(parts, node_list(block(break_lang())))
 
-    head <- node_car(current)
-    switch(as_string(head),
-      # Don't poke within nested loops
-      `repeat` = ,
-      `while` = ,
-      `for` = {
-        next
-      },
-      `next` = {
-        node_poke_car(rest, flow_signaller("flowery_next"))
-        next
-      },
-      `break` = {
-        node_poke_car(rest, flow_signaller("flowery_break"))
-        next
-      },
-      poke_loop_keywords(current)
-    )
-  }
+  # Wrap `while` to disable JIT in case `env` is GlobalEnv
+  expr <- rlang::expr({
+    (`while`)(TRUE, {
+      !! machine_switch_lang(parts)
+    })
+  })
 
-  invisible(node)
-}
+  # Put machine state operators in scope temporarily
+  scoped_bindings(.env = env, `_state` = 1L, !!! control_flow_ops)
 
-flow_signaller <- function(type) {
-  lang(function() cnd_signal(type))
-}
-
-with_loop_flow <- function(expr, env = caller_env()) {
-  tryCatch(
-    flowery_next = function(cnd) "next",
-    flowery_break = function(cnd) "break",
-    {
-      expr
-      "next"
-    }
-  )
+  eval_bare(expr, env)
 }
