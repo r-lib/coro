8f4eec254bfc585f9acc8b4d8aa23bcab8c471ef
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-06-29 10:33:30 -0400

    Add support for iterating over python objects (#37)

--- R/iterator.R
+++ R/iterator.R
@@ -136,12 +136,10 @@ as_iterator.default <- function(x) {
   }
 }
 
-on_load(on_package_load("reticulate", {
-  if (!s3_has_method("coro::as_iterator", "python.builtin.iterator")) {
-    s3_register("coro::as_iterator", "python.builtin.iterator", function(x) {
-      function() {
-        reticulate::iter_next(x, completed = exhausted())
-      }
-    })
-  }
-}))
+
+
+#' @export
+as_iterator.python.builtin.object <- function(x) {
+  x <- reticulate::as_iterator(x)
+  function() reticulate::iter_next(x, exhausted())
+}
--- tests/testthat/test-iterator.R
+++ tests/testthat/test-iterator.R
@@ -21,4 +21,21 @@ def first_n(n):
     out <- c(out, x)
   })
   expect_equal(out, 1:3)
+
+
 })
+
+
+test_that("can use reticulate iteratables", {
+  skip_if_not_installed("reticulate")
+  skip_on_cran()
+  skip_on_ci()
+
+  i <- 0L
+  loop(for (el in reticulate::py_eval("range(3)")) {
+    i <- i + el
+  })
+  expect_equal(i, 3L)
+})
+
+
